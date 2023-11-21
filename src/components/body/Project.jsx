de62267d0df77163434910fcce9d3d51c29d9950
Pablo <cardosopab@gmail.com>
2023-11-20 18:39:22 -0600

    Remove paragraph, and add bulletpoints, and stack

--- src/components/body/Project.jsx
+++ src/components/body/Project.jsx
@@ -1,40 +1,45 @@
-import { createSignal } from 'solid-js';
-import Summary from './Summary';
-import ButtonComponent from './ButtonComponent';
-import Modal from './Modal_Window';
+import { createSignal } from "solid-js";
+import Summary from "./Summary";
+import ButtonComponent from "./ButtonComponent";
+import Modal from "./Modal_Window";
 
 export default function Project(props) {
   const [showModal, setShowModal] = createSignal(false);
   const [modalImage, setModalImage] = createSignal();
 
   function openModal(imageSrc) {
-    console.log('openModal');
+    console.log("openModal");
     console.log(imageSrc);
     // Implement your modal open logic here using the image source (imageSrc)
     setShowModal(true);
     setModalImage(imageSrc);
   }
 
-  const closeModal = (() => setShowModal(false));
+  const closeModal = () => setShowModal(false);
 
   return (
-    <div class='grid grid-cols-1 sm:grid-cols-2 gap-4 py-10 bg-secondary'>
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 py-10 bg-secondary">
       {props.data.map((project) => {
         return (
-          <article id="project-article" class='p-4 flex flex-col'>
+          <article id="project-article" class="p-4 flex flex-col">
             <h2 class="text-text text-center text-3xl sm:text-4xl lg:text-5xl font-bold">
-              <a href={project.title} target="_blank"
+              <a
+                href={project.title}
+                target="_blank"
                 class="text-primary font-bold underline"
-                title={project.title}>{project.name}</a>
+                title={project.title}
+              >
+                {project.name}
+              </a>
             </h2>
             <img
               src={project.images[0]}
-              class='w-80 sm:w-60 md:w-80 lg:w-96 mx-auto my-10 cursor-pointer'
+              class="w-80 sm:w-60 md:w-80 lg:w-96 mx-auto my-10 cursor-pointer"
               alt={project.name}
               loading="lazy"
               onClick={() => openModal(project.images[1])}
             />
-            <div class='flex flex-col w-80 sm:w-60 md:w-80 lg:w-96 mx-auto'>
+            <div class="flex flex-col w-80 sm:w-60 md:w-80 lg:w-96 mx-auto">
               <ButtonComponent
                 live={project.live}
                 link={project.link}
@@ -43,19 +48,15 @@ export default function Project(props) {
               />
               <Summary
                 name={project.name}
-                paragraph={project.paragraph}
-                images={project.images}
+                bulletpoints={project.bulletpoints}
+                stack={project.stack}
               />
             </div>
           </article>
         );
       })}
       {/* Modal section placeholder */}
-      {showModal() &&
-        <Modal
-          image={modalImage()}
-          closeModal={closeModal}
-        />}
+      {showModal() && <Modal image={modalImage()} closeModal={closeModal} />}
     </div>
   );
 }
--- src/components/body/Summary.jsx
+++ src/components/body/Summary.jsx
@@ -1,11 +1,24 @@
 export default function Summary(props) {
-    return (
-        <>
+  return (
+    <>
+      <div class="mt-10 ">
+        {props.bulletpoints.map((point, index) => {
+          return <li key={index}>{point}</li>;
+        })}
 
-            <div class="lg:text-lg mt-10">
-                <p class="text-text" innerHTML={props.paragraph}>
-                </p>
-            </div>
-        </>
-    )
+        <div class="mt-2 flex flex-wrap">
+          <p class="text-primary font-bold pr-2">Stack:</p>
+          {props.stack.map((item, index) => {
+            return (
+              <span key={index}>
+                <a href={item.url} class="text-primary pr-2 underline">
+                  {item.name}
+                </a>
+              </span>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
